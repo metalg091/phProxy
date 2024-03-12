f43a7e2bbec98f1ff87afdd3f8f90343a0251edf
Fürész Tibor Teodor <jaoycb@inf.elte.hu>
2024-03-12 11:43:49 +0100

    Reroute every link

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
-$var = file_get_contents("http://people.inf.elte.hu/pgm6rw");
-echo $var;
+// target -> http://people.inf.elte.hu/pgm6rw
+$var = file_get_contents($_GET["url"]);
+$search = array('src="', 'href"');
+$replace = array('src="furtib.web.elte.hu/phProxy/index.php?url=' + $_GET["url"] + '/', 'href="furtib.web.elte.hu/phProxy/index.php?url=' + $_GET["url"] + '/');
+echo str_replace($search,$replace,$var);
 ?>
